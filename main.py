5817aab0f79393b3f84beddf71ec0d16bb1decdb
eospra59 <60397443+eospra59@users.noreply.github.com>
2020-04-22 09:27:47 -0400

    Update main.py

--- main.py
+++ main.py
@@ -3,6 +3,9 @@ import random
 
 win = pygame.display.set_mode((800, 600))
 img = pygame.image.load('assets/gfx/Overworld.png').convert()
+caveimg = pygame.image.load('assets/gfx/cave.png').convert()
+treeimg = pygame.image.load('assets/gfx/objects.png').convert()
+
 
 grass = pygame.Surface([32, 30]).convert()
 grass.blit(img, (0, 0), (0, 145, 32, 30))
@@ -10,6 +13,10 @@ water = pygame.Surface([32, 30]).convert()
 water.blit(img, (0, 0), (0, 17, 32, 30))
 pit = pygame.Surface([32, 30]).convert()
 pit.blit(img, (0, 0), (240, 370, 32, 30))
+rock = pygame.Surface([32, 30]).convert()
+rock.blit(caveimg, (0, 0), (145, 115, 32, 30))
+tree = pygame.Surface([32, 30]).convert()
+tree.blit(treeimg, (0, 0), (190, 190, 32, 30))
 
 row = []
 for j in range(25):
@@ -19,6 +26,7 @@ for j in range(25):
         tempRow.append(num)
     row.append(tempRow)
     
+ter = input("What type of terrain would you like?")
 
 run = True
 while run:
@@ -28,17 +36,25 @@ while run:
 
     win.fill((0, 0, 0))
 
+    if ter == "ocean":
+        for i in range(25):
+            for j in range(25):
+                if row[i][j] == 0:
+                    win.blit(water, (i * 32, j * 30))
+                if row[i][j] == 1:
+                    win.blit(water, (i * 32, j * 30))
+                if row[i][j] == 2:
+                    win.blit(rock, (i * 32, j * 30))
 
-
-    for i in range(25):
-        for j in range(25):
-            if row[i][j] == 0:
-                win.blit(grass, (i * 32, j * 30))
-            if row[i][j] == 1:
-                win.blit(water, (i * 32, j * 30))
-            if row[i][j] == 2:
-                win.blit(pit, (i * 32, j * 30))
-            
+    if ter == "forest":
+        for i in range(25):
+            for j in range(25):
+                if row[i][j] == 0:
+                    win.blit(grass, (i * 32, j * 30))
+                if row[i][j] == 1:
+                    win.blit(tree, (i * 32, j * 30))
+                if row[i][j] == 2:
+                    win.blit(tree, (i * 32, j * 30))        
 
     pygame.display.update()
 
